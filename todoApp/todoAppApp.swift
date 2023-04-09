882af8f5023dfe6f912b9ba5aec369e6ee3156f8
JinEunPark <wlsdmsrnfl@naver.com>
2023-04-09 17:20:49 +0900

    Initial Commit

---
+++ todoApp/todoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoAppApp.swift
+//  todoApp
+//
+//  Created by 박진은 on 2023/04/09.
+//
+
+import SwiftUI
+
+@main
+struct todoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
